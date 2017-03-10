761380f3fb745aa0d3e6840781115b456187735e
Joe Ortiz <josepho011235@gmail.com>
2017-03-09 22:54:28 -0500

    feat: lists in elm

--- main.elm
+++ main.elm
@@ -28,5 +28,35 @@ renderDog : Dog -> String
 renderDog dog =
     dog.name ++ " is " ++ (toString dog.age) ++ " years old."
 
+
+
+
+type alias Person =
+    { name : String
+    , power : Int
+    }
+
+heroes = [
+    { name = "Doctor Strange", power = 120}
+    ,{name = "Spider Man", power = 90}
+    ,{name = "Thor", power = 100}]
+
+names : List Person -> List String
+names peeps = List.map (\peep -> peep.name) peeps
+
+findPerson : String -> List Person -> Maybe Person
+findPerson name peeps = List.foldl
+    (\peep memo -> case memo of
+        Just _ ->
+            memo
+        Nothing ->
+            if peep.name == name then
+                Just peep
+            else
+                Nothing
+    )
+    Nothing
+    peeps
+
 main =
-    text <| renderDog dog
+    text <| toString <| findPerson "Thor" heroes
