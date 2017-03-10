7f4c7d2697821aae2818eaf7c0e1634f5da13c32
Joe Ortiz <josepho011235@gmail.com>
2017-03-09 22:37:49 -0500

    feat: basic function and type sig

--- main.elm
+++ main.elm
@@ -2,4 +2,16 @@ module Main exposing (..)
 
 import Html exposing (text)
 
-main = text "Hello World"
+politely : String -> String
+politely phrase =
+    "Excuse me" ++ phrase
+
+ask : String -> String -> String
+ask thing place =
+    "Is there a " ++ thing ++ " in the " ++ place ++ "?"
+
+askPolitelyAboutCat : String -> String
+askPolitelyAboutCat = politely << (ask "Cat")
+
+main =
+    text <| askPolitelyAboutCat "Hat"
