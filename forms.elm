35865c0ff9327b79a0fc9202183be619c7436f78
Joe Ortiz <josepho011235@gmail.com>
2017-03-11 22:21:50 -0500

    feat:add form with validation

---
+++ forms.elm
@@ -0,0 +1,75 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import String exposing (length)
+import Regex exposing (regex, contains)
+
+main =
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+
+type alias Model =
+  { name : String
+  , password : String
+  , passwordAgain : String
+  }
+
+model : Model
+model =
+  Model "" "" ""
+
+-- UPDATE
+type Msg
+  = Name String
+    | Password String
+    | PasswordAgain String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Name name ->
+      { model | name = name }
+
+    Password password ->
+      { model | password = password }
+
+    PasswordAgain password ->
+      { model | passwordAgain = password }
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ type_ "text", placeholder "Name", onInput Name ] []
+    , input [ type_ "password", placeholder "Password", onInput Password ] []
+    , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , viewValidation model
+    ]
+
+viewValidation : Model -> Html msg
+viewValidation model =
+  let
+    (matchColor, matchMessage) =
+      if model.password == model.passwordAgain then
+        ("green", "OK")
+      else
+        ("red", "Passwords do not match!")
+
+    (lengthColor, lengthMessage) =
+      if length model.password >= 8 then
+        ("green", "Password length is good")
+      else
+        ("red", "Passwords must be atleast 8 in length!")
+
+    (hasUpperColor, hasUpperMessage) =
+        if contains (regex "[A-Z]") model.password then
+          ("green", "Has an upper case")
+        else
+          ("red", "Needs an Upper Case")
+  in
+      div []
+      [ div [ style [("color", matchColor)] ] [text matchMessage]
+      , div [ style [("color", lengthColor)] ] [text lengthMessage]
+      , div [ style [("color", hasUpperColor)] ] [text hasUpperMessage]
+      ]
